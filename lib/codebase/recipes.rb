9d9fd924daf86736201dfc6cf9795c0120a65001
Adam Cooke <adam@atechmedia.com>
2009-10-28 17:59:08 +0000

    re-add recipes

---
+++ lib/codebase/recipes.rb
@@ -0,0 +1,76 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  
+  after "deploy:symlink", :add_deployment_to_codebase
+
+  task :add_deployment_to_codebase do
+    
+    username = `git config codebase.username`.chomp.strip
+    api_key  = `git config codebase.apikey`.chomp.strip
+    
+    regex = /git\@(gitbase|codebasehq)\.com:(.*)\/(.*)\/(.*)\.git/
+    unless m = repository.match(regex)
+      puts "  * \e[31mYour repository URL does not a match a valid CodebaseHQ Clone URL\e[0m"
+    else
+      url = "#{m[2]}.codebasehq.com"
+      project = m[3]
+      repository = m[4]
+    
+      puts "  * \e[44;33mAdding Deployment to your CodebaseHQ account\e[0m"
+      puts "      -  Account......: #{url}"
+      puts "      -  Username.....: #{username}"
+      puts "      -  API Key......: #{api_key[0,10]}..."
+    
+      puts "      -  Project......: #{project}"
+      puts "      -  Repository...: #{repository}"
+          
+
+      environment_to_send = begin
+        env = (self.environment rescue self.rails_env).dup
+        env.gsub!(/\W+/, ' ')
+        env.strip!
+        env.downcase!
+        env.gsub!(/\ +/, '-')
+        
+        puts "      -  Environment..: #{env}" unless env.blank?
+        
+        env
+      rescue
+        ''
+      end
+
+      servers = roles.values.collect{|r| r.servers}.flatten.collect{|s| s.host}.uniq.join(', ') rescue ''
+    
+      puts "      -  Servers......: #{servers}"
+      puts "      -  Revision.....: #{real_revision}"
+      puts "      -  Branch.......: #{branch}"
+
+      xml = []
+      xml << "<deployment>"
+      xml << "<servers>#{servers}</servers>"
+      xml << "<revision>#{real_revision}</revision>"
+      xml << "<environment>#{environment_to_send}</environment>"
+      xml << "<branch>#{branch}</branch>"
+      xml << "</deployment>"
+            
+      require 'net/http'
+      require 'uri'
+      
+      real_url = "http://#{url}/#{project}/#{repository}/deployments"
+      puts "      -  URL..........: #{real_url}"
+      
+      url = URI.parse(real_url)
+      
+      req = Net::HTTP::Post.new(url.path)
+      req.basic_auth(username, api_key)
+      req.add_field('Content-type', 'application/xml')
+      req.add_field('Accept', 'application/xml')
+      res = Net::HTTP.new(url.host, url.port).start { |http| http.request(req, xml.join) }
+      case res
+      when Net::HTTPCreated then puts "  * \e[32mAdded deployment to Codebase\e[0m"
+      else 
+        puts "  * \e[31mSorry, your deployment was not logged in Codebase - please check your config above.\e[0m"
+      end
+    end
+    
+  end
+end
